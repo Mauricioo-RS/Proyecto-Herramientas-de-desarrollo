8b10ff959641fa05f7cbc0212c1839e52b7a094d
Aldair-m13 <aldair.mayta.e@gmail.com>
2026-09-16 12:27:38 -0500

    Agrega VentaService

--- src/main/java/com/proyecto/minimarket/service/VentaService.java
+++ src/main/java/com/proyecto/minimarket/service/VentaService.java
@@ -1,5 +1,34 @@
 package com.proyecto.minimarket.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.minimarket.model.Venta;
+import com.proyecto.minimarket.repository.VentaRepository;
+
+@Service
 public class VentaService {
-    
+
+    @Autowired
+    private VentaRepository ventaRepository;
+
+    public List<Venta> listarVentas() {
+        return ventaRepository.findAll();
+    }
+
+    public Optional<Venta> buscarPorId(Long id) {
+        return ventaRepository.findById(id);
+    }
+
+    public Venta guardarVenta(Venta venta) {
+        // Aquí puedes agregar lógica adicional si necesitas calcular totales
+        return ventaRepository.save(venta);
+    }
+
+    public void eliminarVenta(Long id) {
+        ventaRepository.deleteById(id);
+    }
 }
