1798466410b6940c3fa1aade249ca3def0a5ddaf
Aldair-m13 <aldair.mayta.e@gmail.com>
2026-09-16 12:24:37 -0500

    Actualiza DetalleVentaService

--- src/main/java/com/proyecto/minimarket/service/DetalleVentaService.java
+++ src/main/java/com/proyecto/minimarket/service/DetalleVentaService.java
@@ -1,5 +1,33 @@
 package com.proyecto.minimarket.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.minimarket.model.DetalleVenta;
+import com.proyecto.minimarket.repository.DetalleVentaRepository;
+
+@Service
 public class DetalleVentaService {
-    
+
+    @Autowired
+    private DetalleVentaRepository detalleVentaRepository;
+
+    public List<DetalleVenta> listarDetalles() {
+        return detalleVentaRepository.findAll();
+    }
+
+    public Optional<DetalleVenta> buscarPorId(Long id) {
+        return detalleVentaRepository.findById(id);
+    }
+
+    public DetalleVenta guardarDetalle(DetalleVenta detalleVenta) {
+        return detalleVentaRepository.save(detalleVenta);
+    }
+
+    public void eliminarDetalle(Long id) {
+        detalleVentaRepository.deleteById(id);
+    }
 }
